2b17a4747d16c5374c45f11d340a0e31369e6ead
virginie ponton <pontonvirginie@gmail.com>
2024-08-21 22:12:07 +0200

    hook

--- app/public/wp-content/themes/planty/functions.php
+++ app/public/wp-content/themes/planty/functions.php
@@ -7,6 +7,7 @@ function montheme_supports()
 {
     add_theme_support('menu');
     register_nav_menu('header', 'en tête du menu');
+    register_nav_menu('footer', 'Pied de page');
 }
 
 function theme_enqueue_styles()
@@ -16,3 +17,19 @@ function theme_enqueue_styles()
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css',);
  
 }
+
+
+function afficher_page_admin_pour_utilisateurs_connectes($items, $args) {
+    // Vérifiez si l'utilisateur est connecté
+    if (is_user_logged_in()) {
+        // l'ID réel de la page "Admin"
+        $page_admin_id = 439;
+        $page_admin_link = get_permalink($page_admin_id);
+
+        // Ajoutez le lien "Admin" uniquement pour les utilisateurs connectés
+        $items .= '<li><a href="' . $page_admin_link . '">Admin</a></li>';
+    }
+
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'afficher_page_admin_pour_utilisateurs_connectes', 10, 2);
