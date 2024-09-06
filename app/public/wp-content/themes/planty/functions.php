82884e196b2e9079267b8cbd666992d834084de2
virginie ponton <pontonvirginie@gmail.com>
2024-09-06 08:32:43 +0200

    creation fichier css responsive

--- app/public/wp-content/themes/planty/functions.php
+++ app/public/wp-content/themes/planty/functions.php
@@ -15,7 +15,7 @@ function theme_enqueue_styles()
     // Chargement du style.css du thème parent Twenty Twenty
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css',);
- 
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/responsive.css',);
 }
 /* HOOK UTILISATEURS CONNECTES */
 
